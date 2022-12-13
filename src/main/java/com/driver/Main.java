eaba4bf650f673ec7228fa139e594284587da542
L R T J NAIDU <rudhrateja1999@gmail.com>
2022-12-13 17:15:35 +0530

    one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
+        public void product(int x, int y) { }
+        public void product(int x, int y, int z) {}
+        public void product(double x, double y) {}
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2233,4545.44);
+    }
 }
